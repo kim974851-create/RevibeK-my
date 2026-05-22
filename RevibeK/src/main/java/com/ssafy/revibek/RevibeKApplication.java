5da9c03d71223845aa0ed11f59675fd5d1446c0b
kimHySoo <153412952+kimHySoo@users.noreply.github.com>
2026-05-22 16:33:12 +0900

    chore: 프로젝트 초기 세팅
    0522 시작

---
+++ RevibeK/src/main/java/com/ssafy/revibek/RevibeKApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.revibek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevibeKApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevibeKApplication.class, args);
+	}
+
+}
---
+++ RevibeK/src/test/java/com/ssafy/revibek/RevibeKApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.revibek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevibeKApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
